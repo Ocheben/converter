a25d97cbce2494ee08d07e8ff9cb25d48f5a8af5
Ocheben <ocheben@gmail.com>
2018-06-28 08:57:29 +0100

    IndexedDB

--- idbi.js
+++ idbi.js
@@ -1,11 +1,10 @@
 
 
 
-const apiURL = `https://free.currencyconverterapi.com/api/v5/countries`;   
+const apiURL = `https://free.currencyconverterapi.com/api/v5/currencies`;   
 let countriesCurrencies;
 const dbPromise = idb.open('countries-currencies', 1, upgradeDB => {
-  // Note: we don't use 'break' in this switch statement,
-  // the fall-through behaviour is what we want.
+
   switch (upgradeDB.oldVersion) {
     case 0:
       upgradeDB.createObjectStore('objs', {keyPath: 'id'});
@@ -31,32 +30,23 @@ fetch(apiURL)
   });
 });
 
-
-
-const select = document.querySelector('#currency');
-const url = `https://free.currencyconverterapi.com/api/v5/currencies`;
-fetch(url).then(response => {
-    if (response.status !== 200) {
-        console.warn('Looks like there was a problem. Status Code: ' + response.status );
-        return;
-    }
-    //Examine the text in the response
-    response.json().then(results => {
-        for (const result in results) {
-            for(const id in results[result]){
-                let opt = document.createElement('option');
-                opt.value = results[result][id]["id"];
-                opt.text = `${results[result][id]["currencyName"]}  ${results[result][id]["id"]}  ${results[result][id]["currencySymbol"]}`;
-                document.getElementById('select').append(opt);
-                
-                let opt2 = document.createElement('option');
-                opt2.value = results[result][id]["id"];
-                opt2.text = `${results[result][id]["currencyName"]}  ${results[result][id]["id"]}  ${results[result][id]["currencySymbol"]}`;
-                document.getElementById('select2').append(opt2);
-            }
-        }
+dbPromise.then(db => {
+  return db.transaction('objs')
+    .objectStore('objs').getAll();
+}).then(results => {
+        results.forEach(x=> {
+            let opt = document.createElement('option');
+            let opt2 = document.createElement('option');
+            opt.value = x.id;
+            opt.text = `${x.currencyName} (${x.id})`;
+            
+            opt2 = opt.cloneNode(true);
+            select.add(opt);
+            select2.add(opt2);
+            
+        });
         console.log(results);
     });
-}).catch(function(err) {
-    console.error('Fetch Error -', err);
-});
+
+
+
